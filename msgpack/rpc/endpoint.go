bfc09b8cc1fe73d5467ac0f9d4a1288adb53e9d0
Gary Burd <gary@beagledreams.com>
2016-09-27 14:19:21 -0700

    Change license to Apache 2

--- msgpack/rpc/endpoint.go
+++ msgpack/rpc/endpoint.go
@@ -1,6 +1,16 @@
-// Copyright 2015 Gary Burd. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2016 Gary Burd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // Package rpc implements MessagePack RPC.
 package rpc
@@ -154,6 +164,9 @@ func (e *Endpoint) skip(n int) error {
 var errorType = reflect.ValueOf(new(error)).Elem().Type()
 
 // Register registers handler fn for the specified method name.
+//
+// When servicing a call, the arguments to fn are the values in args followed
+// by the values passed from the peer.
 func (e *Endpoint) Register(method string, fn interface{}, args ...interface{}) error {
 	v := reflect.ValueOf(fn)
 	t := v.Type()
