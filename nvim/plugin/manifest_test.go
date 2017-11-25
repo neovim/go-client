69d8a5b353c4e7c35cf4f70e7666faca1749b4c5
NoahOrberg <noah.orberg.9999@gmail.com>
2017-11-26 01:18:21 +0900

    fixed more readable

--- nvim/plugin/manifest_test.go
+++ nvim/plugin/manifest_test.go
@@ -8,21 +8,33 @@ import (
 func TestReplaceManifest(t *testing.T) {
 
 	t.Run("not written manifest yet", func(t *testing.T) {
-		vimFileContent := `if exists('g:loaded_hello')
-  finish
-endif
-let g:loaded_hello = 1
+		vimFileContentLines := []string{
+			`if exists('g:loaded_hello')`,
+			`  finish`,
+			`endif`,
+			`let g:loaded_hello = 1`,
+			``,
+			`function! s:RequireHello(host) abort`,
+			`  return jobstart(['hello.nvim'], { 'rpc': v:true })`,
+			`endfunction`,
+		}
+		var vimFileContent string
+		for _, l := range vimFileContentLines {
+			vimFileContent = vimFileContent + l + "\n"
+		}
 
-function! s:RequireHello(host) abort
-  return jobstart(['hello.nvim'], { 'rpc': v:true })
-endfunction
-`
-		manifest := `call remote#host#RegisterPlugin('hello.nvim', '0', [
-\ {'type': 'function', 'name': 'Ahello', 'sync': 1, 'opts': {}},
-\ {'type': 'function', 'name': 'Bhello', 'sync': 1, 'opts': {}},
-\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},
-\ ])
-`
+		manifestLines := []string{
+			`call remote#host#RegisterPlugin('hello.nvim', '0', [`,
+			`\ {'type': 'function', 'name': 'Ahello', 'sync': 1, 'opts': {}},`,
+			`\ {'type': 'function', 'name': 'Bhello', 'sync': 1, 'opts': {}},`,
+			`\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},`,
+			`\ ])`,
+		}
+		var manifest string
+		for _, l := range manifestLines {
+			manifest = manifest + l + "\n"
+		}
+
 		host := "hello.nvim"
 		output := replaceManifest(host, []byte(vimFileContent), []byte(manifest))
 		expected := []byte(vimFileContent + manifest)
@@ -32,40 +44,59 @@ endfunction
 	})
 
 	t.Run("already written manifest", func(t *testing.T) {
-		vimFileContent := `if exists('g:loaded_hello')
-  finish
-endif
-let g:loaded_hello = 1
+		vimFileContentLines := []string{
+			`if exists('g:loaded_hello')`,
+			`  finish`,
+			`endif`,
+			`let g:loaded_hello = 1`,
+			``,
+			`function! s:RequireHello(host) abort`,
+			`  return jobstart(['hello.nvim'], { 'rpc': v:true })`,
+			`endfunction`,
+			`call remote#host#RegisterPlugin('hello.nvim', '0', [`,
+			`\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},`,
+			`\ ])`,
+		}
+		var vimFileContent string
+		for _, l := range vimFileContentLines {
+			vimFileContent = vimFileContent + l + "\n"
+		}
 
-function! s:RequireHello(host) abort
-  return jobstart(['hello.nvim'], { 'rpc': v:true })
-endfunction
-call remote#host#RegisterPlugin('hello.nvim', '0', [
-\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},
-\ ])
-`
-		manifest := `call remote#host#RegisterPlugin('hello.nvim', '0', [
-\ {'type': 'function', 'name': 'Ahello', 'sync': 1, 'opts': {}},
-\ {'type': 'function', 'name': 'Bhello', 'sync': 1, 'opts': {}},
-\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},
-\ ])
-`
+		manifestLines := []string{
+			`call remote#host#RegisterPlugin('hello.nvim', '0', [`,
+			`\ {'type': 'function', 'name': 'Ahello', 'sync': 1, 'opts': {}},`,
+			`\ {'type': 'function', 'name': 'Bhello', 'sync': 1, 'opts': {}},`,
+			`\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},`,
+			`\ ])`,
+		}
+		var manifest string
+		for _, l := range manifestLines {
+			manifest = manifest + l + "\n"
+		}
+
 		host := "hello.nvim"
 		output := replaceManifest(host, []byte(vimFileContent), []byte(manifest))
-		expected := `if exists('g:loaded_hello')
-  finish
-endif
-let g:loaded_hello = 1
 
-function! s:RequireHello(host) abort
-  return jobstart(['hello.nvim'], { 'rpc': v:true })
-endfunction
-call remote#host#RegisterPlugin('hello.nvim', '0', [
-\ {'type': 'function', 'name': 'Ahello', 'sync': 1, 'opts': {}},
-\ {'type': 'function', 'name': 'Bhello', 'sync': 1, 'opts': {}},
-\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},
-\ ])
-`
+		expectedLines := []string{
+			`if exists('g:loaded_hello')`,
+			`  finish`,
+			`endif`,
+			`let g:loaded_hello = 1`,
+			``,
+			`function! s:RequireHello(host) abort`,
+			`  return jobstart(['hello.nvim'], { 'rpc': v:true })`,
+			`endfunction`,
+			`call remote#host#RegisterPlugin('hello.nvim', '0', [`,
+			`\ {'type': 'function', 'name': 'Ahello', 'sync': 1, 'opts': {}},`,
+			`\ {'type': 'function', 'name': 'Bhello', 'sync': 1, 'opts': {}},`,
+			`\ {'type': 'function', 'name': 'Hello', 'sync': 1, 'opts': {}},`,
+			`\ ])`,
+		}
+		var expected string
+		for _, l := range expectedLines {
+			expected = expected + l + "\n"
+		}
+
 		if !bytes.Equal(output, []byte(expected)) {
 			t.Errorf("want %s, but get %s", expected, string(output))
 		}
