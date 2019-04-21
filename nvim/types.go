abf9cd9cef0af2990b9811127684ae8ac1439a76
Koichi Shiraishi <zchee.io@gmail.com>
2019-04-22 04:06:39 +0900

    nvim: add some godoc comment and format

--- nvim/types.go
+++ nvim/types.go
@@ -14,6 +14,7 @@
 
 package nvim
 
+// Mode represents a Nvim's current mode.
 type Mode struct {
 	// Mode is the current mode.
 	Mode string `msgpack:"mode"`
@@ -22,6 +23,7 @@ type Mode struct {
 	Blocking bool `msgpack:"blocking"`
 }
 
+// HLAttrs represents a highlight definitions.
 type HLAttrs struct {
 	Bold       bool `msgpack:"bold,omitempty"`
 	Underline  bool `msgpack:"underline,omitempty"`
@@ -33,6 +35,7 @@ type HLAttrs struct {
 	Special    int  `msgpack:"special,omitempty" empty:"-1"`
 }
 
+// Mapping represents a nvim mapping options.
 type Mapping struct {
 	// LHS is the {lhs} of the mapping.
 	LHS string `msgpack:"lhs"`
@@ -66,12 +69,16 @@ type Mapping struct {
 type ClientVersion struct {
 	// Major major version. (defaults to 0 if not set, for no release yet)
 	Major int `msgpack:"major,omitempty" empty:"0"`
+
 	// Minor minor version.
 	Minor int `msgpack:"minor,omitempty"`
+
 	// Patch patch number.
 	Patch int `msgpack:"patch,omitempty"`
+
 	// Prerelease string describing a prerelease, like "dev" or "beta1".
 	Prerelease string `msgpack:"prerelease,omitempty"`
+
 	// Commit hash or similar identifier of commit.
 	Commit string `msgpack:"commit,omitempty"`
 }
@@ -82,12 +89,16 @@ type ClientType string
 const (
 	// RemoteClientType for the client library.
 	RemoteClientType ClientType = "remote"
+
 	// UIClientType for the gui frontend.
 	UIClientType ClientType = "ui"
+
 	// EmbedderClientType for the application using nvim as a component, for instance IDE/editor implementing a vim mode.
 	EmbedderClientType ClientType = "embedder"
+
 	// HostClientType for the plugin host. Typically started by nvim.
 	HostClientType ClientType = "host"
+
 	// PluginClientType for the single plugin. Started by nvim.
 	PluginClientType ClientType = "plugin"
 )
@@ -105,6 +116,7 @@ type ClientMethod struct {
 	// If true, send as a notification.
 	// If false, send as a blocking request.
 	Async bool `msgpack:"async"`
+
 	// NArgs is the number of method arguments.
 	NArgs ClientMethodNArgs `msgpack:"nargs"`
 }
@@ -113,6 +125,7 @@ type ClientMethod struct {
 type ClientMethodNArgs struct {
 	// Min is the minimum number of method arguments.
 	Min int `msgpack:",array"`
+
 	// Max is the maximum number of method arguments.
 	Max int `msgpack:",array"`
 }
@@ -123,8 +136,10 @@ type ClientAttributes map[string]string
 const (
 	// ClientAttributeKeyWebsite Website of client (for instance github repository).
 	ClientAttributeKeyWebsite = "website"
+
 	// ClientAttributeKeyLicense Informal description of the license, such as "Apache 2", "GPLv3" or "MIT".
 	ClientAttributeKeyLicense = "license"
+
 	// ClientoAttributeKeyLogo URI or path to image, preferably small logo or icon. .png or .svg format is preferred.
 	ClientoAttributeKeyLogo = "logo"
 )
@@ -136,12 +151,16 @@ const (
 type Client struct {
 	// Name is short name for the connected client.
 	Name string `msgpack:"name,omitempty"`
+
 	// Version describes the version, with the following possible keys (all optional).
 	Version ClientVersion `msgpack:"version,omitempty"`
+
 	// Type is the client type. Must be one of the ClientType type values.
 	Type ClientType `msgpack:"type,omitempty"`
+
 	// Methods builtin methods in the client.
 	Methods map[string]*ClientMethod `msgpack:"methods,omitempty"`
+
 	// Attributes is informal attributes describing the client.
 	Attributes ClientAttributes `msgpack:"attributes,omitempty"`
 }
@@ -150,44 +169,61 @@ type Client struct {
 type Channel struct {
 	// Stream is the stream underlying the channel.
 	Stream string `msgpack:"stream,omitempty"`
+
 	// Mode is the how data received on the channel is interpreted.
 	Mode string `msgpack:"mode,omitempty"`
+
 	// Pty is the name of pseudoterminal, if one is used (optional).
 	Pty string `msgpack:"pty,omitempty"`
+
 	// Buffer is the buffer with connected terminal instance (optional).
 	Buffer Buffer `msgpack:"buffer,omitempty"`
+
 	// Client is the information about the client on the other end of the RPC channel, if it has added it using nvim_set_client_info (optional).
 	Client *Client `msgpack:"client,omitempty"`
 }
 
+// Process represents a Proc and ProcChildren functions return type.
 type Process struct {
 	// Name is the name of process command.
 	Name string `msgpack:"name,omitempty"`
+
 	// PID is the process ID.
 	PID int `msgpack:"pid,omitempty"`
+
 	// PPID is the parent process ID.
 	PPID int `msgpack:"ppid,omitempty"`
 }
 
+// UI represents a nvim ui options.
 type UI struct {
 	// Height requested height of the UI
 	Height int `msgpack:"height,omitempty"`
+
 	// Width requested width of the UI
 	Width int `msgpack:"width,omitempty"`
+
 	// RGB whether the UI uses rgb colors (false implies cterm colors)
 	RGB bool `msgpack:"rgb,omitempty"`
+
 	// ExtPopupmenu externalize the popupmenu.
 	ExtPopupmenu bool `msgpack:"ext_popupmenu,omitempty"`
+
 	// ExtTabline externalize the tabline.
 	ExtTabline bool `msgpack:"ext_tabline,omitempty"`
+
 	// ExtCmdline externalize the cmdline.
 	ExtCmdline bool `msgpack:"ext_cmdline,omitempty"`
+
 	// ExtWildmenu externalize the wildmenu.
 	ExtWildmenu bool `msgpack:"ext_wildmenu,omitempty"`
+
 	// ExtNewgrid use new revision of the grid events.
 	ExtNewgrid bool `msgpack:"ext_newgrid,omitempty"`
+
 	// ExtHlstate use detailed highlight state.
 	ExtHlstate bool `msgpack:"ext_hlstate,omitempty"`
+
 	// ChannelID channel id of remote UI (not present for TUI)
 	ChannelID int `msgpack:"chan,omitempty"`
 }
