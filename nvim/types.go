620209ee9c3434faf9c742da28c02d22708ed19e
Koichi Shiraishi <zchee.io@gmail.com>
2020-12-31 22:29:38 +0900

    nvim: move QuickfixError and CommandCompletionArgs type to types.go

--- nvim/types.go
+++ nvim/types.go
@@ -1,5 +1,64 @@
 package nvim
 
+// QuickfixError represents an item in a quickfix list.
+type QuickfixError struct {
+	// Buffer number
+	Bufnr int `msgpack:"bufnr,omitempty"`
+
+	// Line number in the file.
+	LNum int `msgpack:"lnum,omitempty"`
+
+	// Search pattern used to locate the error.
+	Pattern string `msgpack:"pattern,omitempty"`
+
+	// Column number (first column is 1).
+	Col int `msgpack:"col,omitempty"`
+
+	// When Vcol is != 0,  Col is visual column.
+	VCol int `msgpack:"vcol,omitempty"`
+
+	// Error number.
+	Nr int `msgpack:"nr,omitempty"`
+
+	// Description of the error.
+	Text string `msgpack:"text,omitempty"`
+
+	// Single-character error type, 'E', 'W', etc.
+	Type string `msgpack:"type,omitempty"`
+
+	// Name of a file; only used when bufnr is not present or it is invalid.
+	FileName string `msgpack:"filename,omitempty"`
+
+	// Valid is non-zero if this is a recognized error message.
+	Valid int `msgpack:"valid,omitempty"`
+
+	// Module name of a module. If given it will be used in quickfix error window instead of the filename.
+	Module string `msgpack:"module,omitempty"`
+}
+
+// CommandCompletionArgs represents the arguments to a custom command line
+// completion function.
+//
+//  :help :command-completion-custom
+type CommandCompletionArgs struct {
+	// ArgLead is the leading portion of the argument currently being completed
+	// on.
+	ArgLead string `msgpack:",array"`
+
+	// CmdLine is the entire command line.
+	CmdLine string
+
+	// CursorPosString is decimal representation of the cursor position in
+	// bytes.
+	CursorPosString int
+}
+
+// CursorPos returns the cursor position.
+func (a *CommandCompletionArgs) CursorPos() int {
+	n := a.CursorPosString
+	return n
+}
+
 // Mode represents a Nvim's current mode.
 type Mode struct {
 	// Mode is the current mode.
