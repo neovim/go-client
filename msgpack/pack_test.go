ad86e31e0dbe4f92a58ab3abb1d6290133fcb9c9
Koichi Shiraishi <zchee.io@gmail.com>
2021-01-04 15:25:21 +0900

    msgpack: fix pack_test.go to parallel table-driven test

--- msgpack/pack_test.go
+++ msgpack/pack_test.go
@@ -7,93 +7,275 @@ import (
 	"testing"
 )
 
-var packTests = []struct {
-	v interface{}
-	h string
-}{
-	{int64(0x0), "00"},
-	{int64(0x1), "01"},
-	{int64(0x7f), "7f"},
-	{int64(0x80), "cc80"},
-	{int64(0x7fff), "cd7fff"},
-	{int64(0x8000), "cd8000"},
-	{int64(0x7fffffff), "ce7fffffff"},
-	{int64(0x80000000), "ce80000000"},
-	{int64(0x7fffffffffffffff), "cf7fffffffffffffff"},
-	{int64(-0x1), "ff"},
-	{int64(-0x20), "e0"},
-	{int64(-0x21), "d0df"},
-	{int64(-0x80), "d080"},
-	{int64(-0x81), "d1ff7f"},
-	{int64(-0x8000), "d18000"},
-	{int64(-0x8001), "d2ffff7fff"},
-	{int64(-0x80000000), "d280000000"},
-	{int64(-0x80000001), "d3ffffffff7fffffff"},
-	{int64(-0x8000000000000000), "d38000000000000000"},
-	{uint64(0x0), "00"},
-	{uint64(0x1), "01"},
-	{uint64(0x7f), "7f"},
-	{uint64(0xff), "ccff"},
-	{uint64(0x100), "cd0100"},
-	{uint64(0xffff), "cdffff"},
-	{uint64(0x10000), "ce00010000"},
-	{uint64(0xffffffff), "ceffffffff"},
-	{uint64(0x100000000), "cf0000000100000000"},
-	{uint64(0xffffffffffffffff), "cfffffffffffffffff"},
-	{nil, "c0"},
-	{true, "c3"},
-	{false, "c2"},
-	{float64(1.23456), "cb3ff3c0c1fc8f3238"},
-	{mapLen(0x0), "80"},
-	{mapLen(0x1), "81"},
-	{mapLen(0xf), "8f"},
-	{mapLen(0x10), "de0010"},
-	{mapLen(0xffff), "deffff"},
-	{mapLen(0x10000), "df00010000"},
-	{mapLen(0xffffffff), "dfffffffff"},
-	{arrayLen(0x0), "90"},
-	{arrayLen(0x1), "91"},
-	{arrayLen(0xf), "9f"},
-	{arrayLen(0x10), "dc0010"},
-	{arrayLen(0xffff), "dcffff"},
-	{arrayLen(0x10000), "dd00010000"},
-	{arrayLen(0xffffffff), "ddffffffff"},
-	{"", "a0"},
-	{"1", "a131"},
-	{"1234567890123456789012345678901", "bf31323334353637383930313233343536373839303132333435363738393031"},
-	{"12345678901234567890123456789012", "d9203132333435363738393031323334353637383930313233343536373839303132"},
-	{[]byte(""), "c400"},
-	{[]byte("1"), "c40131"},
-	{extension{1, ""}, "c70001"},
-	{extension{2, "1"}, "d40231"},
-	{extension{3, "12"}, "d5033132"},
-	{extension{4, "1234"}, "d60431323334"},
-	{extension{5, "12345678"}, "d7053132333435363738"},
-	{extension{6, "1234567890123456"}, "d80631323334353637383930313233343536"},
-	{extension{7, "12345678901234567"}, "c711073132333435363738393031323334353637"},
-}
-
 func TestPack(t *testing.T) {
-tests:
+	t.Parallel()
+
+	packTests := []struct {
+		v  interface{}
+		hs string
+	}{
+		{
+			v:  int64(0x0),
+			hs: "00",
+		},
+		{
+			v:  int64(0x1),
+			hs: "01",
+		},
+		{
+			v:  int64(0x7f),
+			hs: "7f",
+		},
+		{
+			v:  int64(0x80),
+			hs: "cc80",
+		},
+		{
+			v:  int64(0x7fff),
+			hs: "cd7fff",
+		},
+		{
+			v:  int64(0x8000),
+			hs: "cd8000",
+		},
+		{
+			v:  int64(0x7fffffff),
+			hs: "ce7fffffff",
+		},
+		{
+			v:  int64(0x80000000),
+			hs: "ce80000000",
+		},
+		{
+			v:  int64(0x7fffffffffffffff),
+			hs: "cf7fffffffffffffff",
+		},
+		{
+			v:  int64(-0x1),
+			hs: "ff",
+		},
+		{
+			v:  int64(-0x20),
+			hs: "e0",
+		},
+		{
+			v:  int64(-0x21),
+			hs: "d0df",
+		},
+		{
+			v:  int64(-0x80),
+			hs: "d080",
+		},
+		{
+			v:  int64(-0x81),
+			hs: "d1ff7f",
+		},
+		{
+			v:  int64(-0x8000),
+			hs: "d18000",
+		},
+		{
+			v:  int64(-0x8001),
+			hs: "d2ffff7fff",
+		},
+		{
+			v:  int64(-0x80000000),
+			hs: "d280000000",
+		},
+		{
+			v:  int64(-0x80000001),
+			hs: "d3ffffffff7fffffff",
+		},
+		{
+			v:  int64(-0x8000000000000000),
+			hs: "d38000000000000000",
+		},
+		{
+			v:  uint64(0x0),
+			hs: "00",
+		},
+		{
+			v:  uint64(0x1),
+			hs: "01",
+		},
+		{
+			v:  uint64(0x7f),
+			hs: "7f",
+		},
+		{
+			v:  uint64(0xff),
+			hs: "ccff",
+		},
+		{
+			v:  uint64(0x100),
+			hs: "cd0100",
+		},
+		{
+			v:  uint64(0xffff),
+			hs: "cdffff",
+		},
+		{
+			v:  uint64(0x10000),
+			hs: "ce00010000",
+		},
+		{
+			v:  uint64(0xffffffff),
+			hs: "ceffffffff",
+		},
+		{
+			v:  uint64(0x100000000),
+			hs: "cf0000000100000000",
+		},
+		{
+			v:  uint64(0xffffffffffffffff),
+			hs: "cfffffffffffffffff",
+		},
+		{
+			v:  nil,
+			hs: "c0",
+		},
+		{
+			v:  true,
+			hs: "c3",
+		},
+		{
+			v:  false,
+			hs: "c2",
+		},
+		{
+			v:  float64(1.23456),
+			hs: "cb3ff3c0c1fc8f3238",
+		},
+		{
+			v:  mapLen(0x0),
+			hs: "80",
+		},
+		{
+			v:  mapLen(0x1),
+			hs: "81",
+		},
+		{
+			v:  mapLen(0xf),
+			hs: "8f",
+		},
+		{
+			v:  mapLen(0x10),
+			hs: "de0010",
+		},
+		{
+			v:  mapLen(0xffff),
+			hs: "deffff",
+		},
+		{
+			v:  mapLen(0x10000),
+			hs: "df00010000",
+		},
+		{
+			v:  mapLen(0xffffffff),
+			hs: "dfffffffff",
+		},
+		{
+			v:  arrayLen(0x0),
+			hs: "90",
+		},
+		{
+			v:  arrayLen(0x1),
+			hs: "91",
+		},
+		{
+			v:  arrayLen(0xf),
+			hs: "9f",
+		},
+		{
+			v:  arrayLen(0x10),
+			hs: "dc0010",
+		},
+		{
+			v:  arrayLen(0xffff),
+			hs: "dcffff",
+		},
+		{
+			v:  arrayLen(0x10000),
+			hs: "dd00010000",
+		},
+		{
+			v:  arrayLen(0xffffffff),
+			hs: "ddffffffff",
+		},
+		{
+			v:  "",
+			hs: "a0",
+		},
+		{
+			v:  "1",
+			hs: "a131",
+		},
+		{
+			v:  "1234567890123456789012345678901",
+			hs: "bf31323334353637383930313233343536373839303132333435363738393031",
+		},
+		{
+			v:  "12345678901234567890123456789012",
+			hs: "d9203132333435363738393031323334353637383930313233343536373839303132",
+		},
+		{
+			v:  []byte(""),
+			hs: "c400",
+		},
+		{
+			v:  []byte("1"),
+			hs: "c40131",
+		},
+		{
+			v:  extension{1, ""},
+			hs: "c70001",
+		},
+		{
+			v:  extension{2, "1"},
+			hs: "d40231",
+		},
+		{
+			v:  extension{3, "12"},
+			hs: "d5033132",
+		},
+		{
+			v:  extension{4, "1234"},
+			hs: "d60431323334",
+		},
+		{
+			v:  extension{5, "12345678"},
+			hs: "d7053132333435363738",
+		},
+		{
+			v:  extension{6, "1234567890123456"},
+			hs: "d80631323334353637383930313233343536",
+		},
+		{
+			v:  extension{7, "12345678901234567"},
+			hs: "c711073132333435363738393031323334353637",
+		},
+	}
 	for _, tt := range packTests {
+		tt := tt
+		t.Run(fmt.Sprintf("%s", tt.v), func(t *testing.T) {
+			t.Parallel()
 
-		var arg string
-		switch reflect.ValueOf(tt.v).Kind() {
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			arg = fmt.Sprintf("%T %x", tt.v, tt.v)
-		default:
-			arg = fmt.Sprintf("%T %v", tt.v, tt.v)
-		}
+			var arg string
+			switch reflect.ValueOf(tt.v).Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+				arg = fmt.Sprintf("%T %x", tt.v, tt.v)
+			default:
+				arg = fmt.Sprintf("%T %v", tt.v, tt.v)
+			}
 
-		p, err := pack(tt.v)
-		if err != nil {
-			t.Errorf("pack %s returned error %v", arg, err)
-			continue tests
-		}
-		h := hex.EncodeToString(p)
-		if h != tt.h {
-			t.Errorf("pack %s returned %s, want %s", arg, h, tt.h)
-			continue tests
-		}
+			p, err := pack(tt.v)
+			if err != nil {
+				t.Fatalf("pack %s returned error %v", arg, err)
+			}
+			h := hex.EncodeToString(p)
+			if h != tt.hs {
+				t.Fatalf("pack %s returned %s, want %s", arg, h, tt.hs)
+			}
+		})
 	}
 }
