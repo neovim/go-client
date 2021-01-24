10087bb18973c7661585a0d18b0364dca0b55bbd
Koichi Shiraishi <zchee.io@gmail.com>
2021-01-24 21:05:43 +0900

    nvim: add BinaryName constant for unix and windows GOOS
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- nvim/nvim_windows.go
+++ nvim/nvim_windows.go
@@ -1,6 +1,13 @@
+// +build windows
+
 package nvim
 
-import "syscall"
+import (
+	"syscall"
+)
+
+// BinaryName is the name of default nvim binary name.
+const BinaryName = "nvim.exe"
 
 func init() {
 	embedProcAttr = &syscall.SysProcAttr{HideWindow: true}
