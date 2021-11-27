172bd627dac31ae8f936a537425556aff5aa6bb7
Koichi Shiraishi <zchee.io@gmail.com>
2021-11-28 03:49:10 +0900

    nvim: go generate
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- nvim/api_deprecated.go
+++ nvim/api_deprecated.go
@@ -67,3 +67,112 @@ func (b *Batch) ExecuteLua(code string, result interface{}, args ...interface{})
 	}
 	b.call("nvim_execute_lua", result, code, args)
 }
+
+// CommandOutput executes a single ex command and returns the output.
+//
+// Deprecated: Use Exec instead.
+func (v *Nvim) CommandOutput(cmd string) (out string, err error) {
+	err = v.call("nvim_command_output", &out, cmd)
+	return out, err
+}
+
+// CommandOutput executes a single ex command and returns the output.
+//
+// Deprecated: Use Exec instead.
+func (b *Batch) CommandOutput(cmd string, out *string) {
+	b.call("nvim_command_output", out, cmd)
+}
+
+// BufferNumber gets a buffer's number.
+//
+// Deprecated: Use int(buffer) to get the buffer's number as an integer.
+func (v *Nvim) BufferNumber(buffer Buffer) (number int, err error) {
+	err = v.call("nvim_buf_get_number", &number, buffer)
+	return number, err
+}
+
+// BufferNumber gets a buffer's number.
+//
+// Deprecated: Use int(buffer) to get the buffer's number as an integer.
+func (b *Batch) BufferNumber(buffer Buffer, number *int) {
+	b.call("nvim_buf_get_number", number, buffer)
+}
+
+// ClearBufferHighlight clears highlights from a given source group and a range
+// of lines.
+//
+// To clear a source group in the entire buffer, pass in 1 and -1 to startLine
+// and endLine respectively.
+//
+// The lineStart and lineEnd parameters specify the range of lines to clear.
+// The end of range is exclusive. Specify -1 to clear to the end of the file.
+//
+// Deprecated: Use ClearBufferNamespace instead.
+func (v *Nvim) ClearBufferHighlight(buffer Buffer, srcID int, startLine int, endLine int) error {
+	return v.call("nvim_buf_clear_highlight", nil, buffer, srcID, startLine, endLine)
+}
+
+// ClearBufferHighlight clears highlights from a given source group and a range
+// of lines.
+//
+// To clear a source group in the entire buffer, pass in 1 and -1 to startLine
+// and endLine respectively.
+//
+// The lineStart and lineEnd parameters specify the range of lines to clear.
+// The end of range is exclusive. Specify -1 to clear to the end of the file.
+//
+// Deprecated: Use ClearBufferNamespace instead.
+func (b *Batch) ClearBufferHighlight(buffer Buffer, srcID int, startLine int, endLine int) {
+	b.call("nvim_buf_clear_highlight", nil, buffer, srcID, startLine, endLine)
+}
+
+// SetBufferVirtualText set the virtual text (annotation) for a buffer line.
+//
+// By default (and currently the only option), the text will be placed after
+// the buffer text.
+//
+// Virtual text will never cause reflow, rather virtual text will be truncated at the end of the screen line.
+// The virtual text will begin one cell (|lcs-eol| or space) after the ordinary text.
+//
+// Namespaces are used to support batch deletion/updating of virtual text.
+// To create a namespace, use CreateNamespace. Virtual text is cleared using ClearBufferNamespace.
+//
+// The same nsID can be used for both virtual text and highlights added by AddBufferHighlight,
+// both can then be cleared with a single call to ClearBufferNamespace.
+// If the virtual text never will be cleared by an API call, pass `nsID = -1`.
+//
+// As a shorthand, `nsID = 0` can be used to create a new namespace for the
+// virtual text, the allocated id is then returned.
+//
+// The opts arg is reserved for future use.
+//
+// Deprecated: Use SetBufferExtmark instead.
+func (v *Nvim) SetBufferVirtualText(buffer Buffer, nsID int, line int, chunks []TextChunk, opts map[string]interface{}) (id int, err error) {
+	err = v.call("nvim_buf_set_virtual_text", &id, buffer, nsID, line, chunks, opts)
+	return id, err
+}
+
+// SetBufferVirtualText set the virtual text (annotation) for a buffer line.
+//
+// By default (and currently the only option), the text will be placed after
+// the buffer text.
+//
+// Virtual text will never cause reflow, rather virtual text will be truncated at the end of the screen line.
+// The virtual text will begin one cell (|lcs-eol| or space) after the ordinary text.
+//
+// Namespaces are used to support batch deletion/updating of virtual text.
+// To create a namespace, use CreateNamespace. Virtual text is cleared using ClearBufferNamespace.
+//
+// The same nsID can be used for both virtual text and highlights added by AddBufferHighlight,
+// both can then be cleared with a single call to ClearBufferNamespace.
+// If the virtual text never will be cleared by an API call, pass `nsID = -1`.
+//
+// As a shorthand, `nsID = 0` can be used to create a new namespace for the
+// virtual text, the allocated id is then returned.
+//
+// The opts arg is reserved for future use.
+//
+// Deprecated: Use SetBufferExtmark instead.
+func (b *Batch) SetBufferVirtualText(buffer Buffer, nsID int, line int, chunks []TextChunk, opts map[string]interface{}, id *int) {
+	b.call("nvim_buf_set_virtual_text", id, buffer, nsID, line, chunks, opts)
+}
