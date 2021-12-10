34b6240505fdfad5affef98127328a3a7dc86470
Koichi Shiraishi <zchee.io@gmail.com>
2021-12-11 07:10:40 +0900

    nvim/plugin: use sub-tests
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- nvim/plugin/manifest_test.go
+++ nvim/plugin/manifest_test.go
@@ -10,62 +10,70 @@ const (
 	updatedManifest  = "call remote#host#RegisterPlugin('P', '0', [\n\\ {'type': 'function', 'name': 'Bar', 'sync': 1, 'opts': {}},\n\\ ])\n"
 )
 
-var replaceManifestTests = []struct {
-	what, original, expected string
-}{
-	{
-		"Original at beginning of file",
-		fmt.Sprintf("%sline A\nline B\n", originalManifest),
-		fmt.Sprintf("%sline A\nline B\n", updatedManifest),
-	},
-	{
-		"Original in middle of file",
-		fmt.Sprintf("line A\n%sline B\n", originalManifest),
-		fmt.Sprintf("line A\n%sline B\n", updatedManifest),
-	},
-	{
-		"Original at end of file",
-		fmt.Sprintf("line A\nline B\n%s", originalManifest),
-		fmt.Sprintf("line A\nline B\n%s", updatedManifest),
-	},
-	{
-		"Original at end of file, no trailing \\n",
-		fmt.Sprintf("line A\nline B\n%s", originalManifest[:len(originalManifest)-1]),
-		fmt.Sprintf("line A\nline B\n%s", updatedManifest),
-	},
-	{
-		"No manifest",
-		"line A\nline B\n",
-		fmt.Sprintf("line A\nline B\n%s", updatedManifest),
-	},
-	{
-		"Empty file",
-		"",
-		updatedManifest,
-	},
-	{
-		"No manifest, no trailing \\n",
-		"line A\nline B",
-		fmt.Sprintf("line A\nline B\n%s", updatedManifest),
-	},
-	{
-		"Extra \\ ])` in file", // ensure non-greedy match trailing ])
-		fmt.Sprintf("line A\n%sline B\n\\ ])\nline C\n", originalManifest),
-		fmt.Sprintf("line A\n%sline B\n\\ ])\nline C\n", updatedManifest),
-	},
-}
-
 func TestReplaceManifest(t *testing.T) {
+	t.Parallel()
+
+	var replaceManifestTests = []struct {
+		name     string
+		original string
+		expected string
+	}{
+		{
+			name:     "Original at beginning of file",
+			original: fmt.Sprintf("%sline A\nline B\n", originalManifest),
+			expected: fmt.Sprintf("%sline A\nline B\n", updatedManifest),
+		},
+		{
+			name:     "Original in middle of file",
+			original: fmt.Sprintf("line A\n%sline B\n", originalManifest),
+			expected: fmt.Sprintf("line A\n%sline B\n", updatedManifest),
+		},
+		{
+			name:     "Original at end of file",
+			original: fmt.Sprintf("line A\nline B\n%s", originalManifest),
+			expected: fmt.Sprintf("line A\nline B\n%s", updatedManifest),
+		},
+		{
+			name:     "Original at end of file, no trailing \\n",
+			original: fmt.Sprintf("line A\nline B\n%s", originalManifest[:len(originalManifest)-1]),
+			expected: fmt.Sprintf("line A\nline B\n%s", updatedManifest),
+		},
+		{
+			name:     "No manifest",
+			original: "line A\nline B\n",
+			expected: fmt.Sprintf("line A\nline B\n%s", updatedManifest),
+		},
+		{
+			name:     "Empty file",
+			original: "",
+			expected: updatedManifest,
+		},
+		{
+			name:     "No manifest, no trailing \\n",
+			original: "line A\nline B",
+			expected: fmt.Sprintf("line A\nline B\n%s", updatedManifest),
+		},
+		{
+			name:     "Extra \\ ])` in file", // ensure non-greedy match trailing ])
+			original: fmt.Sprintf("line A\n%sline B\n\\ ])\nline C\n", originalManifest),
+			expected: fmt.Sprintf("line A\n%sline B\n\\ ])\nline C\n", updatedManifest),
+		},
+	}
 	for _, tt := range replaceManifestTests {
-		actual := string(replaceManifest("P", []byte(tt.original), []byte(updatedManifest)))
-		if actual != tt.expected {
-			t.Errorf("%s\n got = %q\nwant = %q", tt.what, actual, tt.expected)
-			continue
-		}
-		// Replace should be idempotent.
-		actual = string(replaceManifest("P", []byte(tt.expected), []byte(updatedManifest)))
-		if actual != tt.expected {
-			t.Errorf("%s (no change expected)\n got = %q\nwant = %q", tt.what, actual, tt.expected)
-		}
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			actual := string(replaceManifest("P", []byte(tt.original), []byte(updatedManifest)))
+			if actual != tt.expected {
+				t.Fatalf("%s\n got = %q\nwant = %q", tt.name, actual, tt.expected)
+			}
+
+			// Replace should be idempotent.
+			actual = string(replaceManifest("P", []byte(tt.expected), []byte(updatedManifest)))
+			if actual != tt.expected {
+				t.Fatalf("%s (no change expected)\n got = %q\nwant = %q", tt.name, actual, tt.expected)
+			}
+		})
 	}
 }
